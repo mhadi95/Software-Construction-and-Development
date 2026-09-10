946dbc21463b089847c0c71c36d624e3675eeef0
Muhammad Hadi <67646242+mhadi95@users.noreply.github.com>
2026-09-10 22:16:04 +0500

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,13 @@
+print('Leap Year Checker')
+
+while True:
+    try:
+        year = int(input('Enter a year:'))
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(str(year) + " is a leap year")
+        else:
+            print(str(year) + " is not a leap year")
+        break
+
+    except ValueError:
+        print("Invalid input. Please try again.")
